742ad29b2cc9ae4367e12028a866a7cebe589793
yordan.petrov <yordan.petrov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-01-22 13:16:44 +0000

    Changeset: * Refactored FakeServiceAnswer; * Refactored ServiceRequestHandler; * Refactored ServiceCommunicator; * Renamed some of the errors marking service connection failure.
    Initial import:
    * AtmosphereGesturePlayer project representing an UIAutomator based gesture player using sockets to communicate with the Agent;
    * Ant scripts for building the gesture player project;
    * Anchor and Timeline classes used for passing gestures to the gesture player;
    * GesturePlayerConstants;
    * OnDeviceComponentRequestHandler abstract class;
    * GesturePlayerRequestHandler and GesturePlayerCommunicator classes;
    * FakeOnDeviceComponentAnswer used for fake component response in the unit and integration tests.
    
    [http://jenkins.musala.com/r/6523/ $6523] see #2616
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9599 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ src/com/musala/atmosphere/commons/ad/gestureplayer/GesturePlayerConstants.java
@@ -0,0 +1,12 @@
+package com.musala.atmosphere.commons.ad.gestureplayer;
+
+/**
+ * Constants that are used in Agent-GesturePlayer communication.
+ * 
+ * @author yordan.petrov
+ * 
+ */
+public class GesturePlayerConstants
+{
+	public static final int PLAYER_PORT = 6750;
+}
---
+++ src/com/musala/atmosphere/commons/ad/gestureplayer/Timeline.java
@@ -0,0 +1,12 @@
+package com.musala.atmosphere.commons.ad.gestureplayer;
+
+import java.util.LinkedList;
+
+public class Timeline extends LinkedList<Anchor>
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4037933611110545542L;
+}
