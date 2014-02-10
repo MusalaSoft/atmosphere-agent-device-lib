5763ef3aec61dbf0bbfede46bb3c436b047301a4
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-10 09:47:40 +0000

    Changeset: * Refactored some class names (GesturePlayer -> UIAutomatorBridge); * Implemented logic behind gesture Client->Device transfer; * Moved several classes to their corresponding packages; * Fixed some project setup files; * Exception names refactoring; * Fixed a bug in the uiautomator background execution.
    [http://jenkins.musala.com/r/6740/ $6740] see #2754
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9648 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ src/com/musala/atmosphere/commons/ad/uiautomator/UIAutomatorBridgeConstants.java
@@ -0,0 +1,12 @@
+package com.musala.atmosphere.commons.ad.uiautomator;
+
+/**
+ * Constants that are used in Agent-UIAutomator bridge communication.
+ * 
+ * @author yordan.petrov
+ * 
+ */
+public class UIAutomatorBridgeConstants
+{
+	public static final int BRIDGE_PORT = 6750;
+}
---
+++ src/com/musala/atmosphere/commons/ad/uiautomator/UIAutomatorBridgeRequest.java
@@ -0,0 +1,28 @@
+package com.musala.atmosphere.commons.ad.uiautomator;
+
+import com.musala.atmosphere.commons.ad.RequestType;
+import com.musala.atmosphere.commons.gesture.Timeline;
+
+/**
+ * Enumerators that define the type of action that is requested from the ATMOSPHERE UIAutomator bridge.
+ * 
+ * @author yordan.petrov
+ * 
+ */
+public enum UIAutomatorBridgeRequest implements RequestType
+{
+	/**
+	 * Used in order to validate the bridge. The bridge should return this object back as a response.
+	 */
+	VALIDATION,
+
+	/**
+	 * Returned by the bridge when the the request does not require a particular response.
+	 */
+	ANY_RESPONSE,
+
+	/**
+	 * Plays the gesture, described in the passed {@link Timeline} list.
+	 */
+	PLAY_GESTURE;
+}
