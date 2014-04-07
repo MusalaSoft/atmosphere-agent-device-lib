c409c5654b1642eefe0932006366b5401dd3fe35
delyan.dimitrov <delyan.dimitrov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-04-07 06:58:14 +0000

    Added an application starting mechanism that uses the ATMOSPHERE service.
    [http://jenkins.musala.com/r/7643/ $7643] see #2973
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9850 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/ad/service/ServiceRequest.java
+++ src/com/musala/atmosphere/commons/ad/service/ServiceRequest.java
@@ -9,45 +9,49 @@ import com.musala.atmosphere.commons.ad.RequestType;
  * @author yordan.petrov
  * 
  */
-public enum ServiceRequest implements RequestType
-{
-	/**
-	 * Used in order to validate the ATMOSPHERE service. The service should return this object back as a response.
-	 */
-	VALIDATION,
+public enum ServiceRequest implements RequestType {
+    /**
+     * Used in order to validate the ATMOSPHERE service. The service should return this object back as a response.
+     */
+    VALIDATION,
 
-	/**
-	 * Used to request a {@link PowerProperties} data container, describing the device power environment.
-	 */
-	GET_POWER_PROPERTIES,
+    /**
+     * Used to request a {@link PowerProperties} data container, describing the device power environment.
+     */
+    GET_POWER_PROPERTIES,
 
-	/**
-	 * Used to request setting On the WiFi state of the device.
-	 */
-	SET_WIFI,
+    /**
+     * Used to request setting On the WiFi state of the device.
+     */
+    SET_WIFI,
 
-	/**
-	 * Used to request information about the connection type of the device.
-	 */
-	GET_CONNECTION_TYPE,
+    /**
+     * Used to request information about the connection type of the device.
+     */
+    GET_CONNECTION_TYPE,
 
-	/**
-	 * Returned by the service when the the request does not require a particular response.
-	 */
-	ANY_RESPONSE,
+    /**
+     * Returned by the service when the the request does not require a particular response.
+     */
+    ANY_RESPONSE,
 
-	/**
-	 * Sent to request the readings from the orientation sensor of the device.
-	 */
-	GET_ORIENTATION_READINGS,
+    /**
+     * Sent to request the readings from the orientation sensor of the device.
+     */
+    GET_ORIENTATION_READINGS,
 
-	/**
-	 * Sent to request the readings from the acceleration sensor of the device.
-	 */
-	GET_ACCELERATION_READINGS,
+    /**
+     * Sent to request the readings from the acceleration sensor of the device.
+     */
+    GET_ACCELERATION_READINGS,
 
-	/*
-	 * Sent to request the information telephony services on the device.
-	 */
-	GET_TELEPHONY_INFORMATION;
+    /**
+     * Sent to request the information telephony services on the device.
+     */
+    GET_TELEPHONY_INFORMATION,
+
+    /**
+     * Sent to request an application launch on the device.
+     */
+    START_APP;
 }
